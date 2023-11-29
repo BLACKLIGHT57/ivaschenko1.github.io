a0667b0fefbbb0c0f060164077ae4749018cd8e7
BLACKLIGHT57 <144099991+BLACKLIGHT57@users.noreply.github.com>
2023-11-29 10:05:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -20,3 +20,19 @@ $(document).ready(function(){
         ]
     });
     });
+    $(document).ready(function(){
+        //Скрыть PopUp при загрузке страницы    
+        PopUpHide();
+    });
+    //Функция отображения PopUp
+    function PopUpShow(){
+        $("#popup1").show();
+        window.history.pushState("8",null,' ')
+    }
+    //Функция скрытия PopUp
+    function PopUpHide(){
+        $("#popup1").hide();
+    }
+    $(window).on('popstate', function () {
+        PopUpHide();
+    });
